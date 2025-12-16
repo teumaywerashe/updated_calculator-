54031388acd8f938de2d9f1906b4572f0780772c
teumaywerashe <teumay21@gmail.com>
2025-12-16 20:06:42 +0300

    refactor: clean up CSS comments and improve button event handling in JavaScript

--- calculator.js
+++ calculator.js
@@ -1,31 +1,47 @@
-let result = JSON.parse(localStorage.getItem("result")) || "";
+let result = "";
 document.getElementById("p").innerText = result;
+
+const buttons = document.querySelectorAll(".button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        if (e.target.innerText === "=") {
+            showResult();
+        } else if (e.target.innerHTML === "AC") {
+            clearResult();
+        } else if (e.target.innerHTML === '⌫') {
+            backspace();
+        } else {
+            append(e.target.innerHTML);
+        }
+    });
+});
 
 function append(value) {
     result += value;
     document.getElementById("p").textContent = result;
-    localStorage.setItem("result", JSON.stringify(result))
+    localStorage.setItem("result", JSON.stringify(result));
+    console.log(typeof result);
 }
 
 function clearResult() {
     result = "";
-    localStorage.removeItem("result")
+    localStorage.removeItem("result");
     document.getElementById("p").innerText = result;
 }
 
 function backspace() {
     result = result.slice(0, -1);
     document.getElementById("p").innerText = result;
-
 }
 
 function showResult() {
     try {
-        result = eval(result)
+        result = eval(result);
         document.getElementById("p").innerText = result;
     } catch {
         document.getElementById("p").innerText = "error";
-        result = ""
+        // result = "";
     }
-    localStorage.setItem("result", JSON.stringify(result))
+    // localStorage.setItem("result", JSON.stringify(result));
 }
